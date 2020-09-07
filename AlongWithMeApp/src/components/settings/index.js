0cdd9b0185bc01d3e0012911833eda0fda38df98
AnnaxGrace <anna.grace.conover@gmail.com>
2020-09-07 12:51:27 -0500

    uploaded all fire sounds, and they all play with a start/stop button

--- AlongWithMeApp/src/components/settings/index.js
+++ AlongWithMeApp/src/components/settings/index.js
@@ -4,6 +4,11 @@ import Slider from "@react-native-community/slider"
 import { Picker } from "@react-native-community/picker"
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import RainSound1 from "../../assets/rain_choiceone.mp3"
+import FireSound1 from "../../assets/fire_one.mp3"
+import FireSound2 from "../../assets/fire_two.mp3"
+import FireSound3 from "../../assets/fire_three.mp3"
+
+
 var Sound = require("react-native-sound");
 import {
   SafeAreaView,
@@ -23,7 +28,22 @@ const sounds = [
     title:"mp3 in bundle",
     url: RainSound1,
     basePath: Sound.MAIN_BUNDLE
-  }
+  },
+  {
+    title:"mp3 for fire1",
+    url: FireSound1,
+    basePath: Sound.MAIN_BUNDLE
+  },
+  {
+    title:"mp3 for fire2",
+    url: FireSound2,
+    basePath: Sound.MAIN_BUNDLE
+  },
+  {
+    title:"mp3 for fire2",
+    url: FireSound3,
+    basePath: Sound.MAIN_BUNDLE
+  },
 ]
 
 // Auto linked react-native-sound, made sure path to sound file was correct, changed file name to path + name, Currently using walking through bushes sound, added volume control
@@ -42,9 +62,60 @@ var rain1 = new Sound(RainSound1, Sound.MAIN_BUNDLE, (error) => {
     }
   })
 })
+
+var fire1 = new Sound(FireSound1, Sound.MAIN_BUNDLE, (error) => {
+  if (error) {
+    console.warn("failed to load the sound", error);
+    return;
+  }
+  console.warn("it loaded")
+  fire1.play((success) => {
+    if (success) {
+      console.warn("success")
+    }
+    else {
+      console.warn("it fail")
+    }
+  })
+})
+
+var fire2 = new Sound(FireSound2, Sound.MAIN_BUNDLE, (error) => {
+  if (error) {
+    console.warn("failed to load the sound", error);
+    return;
+  }
+  console.warn("it loaded")
+  fire2.play((success) => {
+    if (success) {
+      console.warn("success")
+    }
+    else {
+      console.warn("it fail")
+    }
+  })
+})
+
+var fire3 = new Sound(FireSound3, Sound.MAIN_BUNDLE, (error) => {
+  if (error) {
+    console.warn("failed to load the sound", error);
+    return;
+  }
+  console.warn("it loaded")
+  fire2.play((success) => {
+    if (success) {
+      console.warn("success")
+    }
+    else {
+      console.warn("it fail")
+    }
+  })
+})
 //volume control
 rain1.setVolume(1);
-//blah melk
+fire1.setVolume(50)
+fire2.setVolume(50)
+fire3.setVolume(50)
+
 class SettingsComponent extends Component {
   state = {
     rain: 'pick your choice',
@@ -57,6 +128,15 @@ class SettingsComponent extends Component {
   playRainSound()  {
       rain1.play()
       console.warn("hi")
+  }
+
+  playFireSound() {
+    fire3.play()
+    console.warn("button runs")
+  }
+
+  stopFireSound() {
+    fire3.stop()
   }
   render() {
     return (
@@ -86,7 +166,13 @@ class SettingsComponent extends Component {
               <Picker.Item label="Rain 3" value="Rain3" />
               <Picker.Item label="Randomize" value="RainRandom" />
             </Picker>
-            <Text>Forest</Text>
+            <Text>Forest
+              <Button
+                onPress={this.playFireSound}
+                title=":sound:"
+                accessibilityLabel="Button to play fire sound"
+              />
+            </Text>
             <Picker
               selectedValue={this.state.forest}
               style={{height: 50, width: 150}, styles.pickers}
@@ -98,7 +184,18 @@ class SettingsComponent extends Component {
               <Picker.Item label="Forest 3" value="Forest3" />
               <Picker.Item label="Randomize" value="ForestRandom" />
             </Picker>
-            <Text>Fireplace</Text>
+            <Text>Fire
+              <Button
+                onPress={this.playFireSound}
+                title=":sound:"
+                accessibilityLabel="Button to play fire sound"
+              />
+              <Button
+                onPress={this.stopFireSound}
+                title=":stopsound:"
+                accessibilityLabel="Button to stop fire sound"
+              />
+            </Text>
             <Picker
               selectedValue={this.state.fireplace}
               style={{height: 50, width: 150}, styles.pickers}
